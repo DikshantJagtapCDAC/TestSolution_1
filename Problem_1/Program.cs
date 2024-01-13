0ac7c74028ac07c6c4f6073e3f3b7770e91d40ea
Dikshant Jagtap <118341759+DikshantJagtapCDAC@users.noreply.github.com>
2024-01-13 10:37:39 +0530

    Sol_1

---
+++ Problem_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        HashSet<string> distinctTerms = new HashSet<string>();
+
+        for (int a = 2; a <= 100; a++)
+        {
+            for (int b = 2; b <= 100; b++)
+            {
+                distinctTerms.Add(BigInteger.Pow(a, b).ToString());
+            }
+        }
+
+        int result = distinctTerms.Count;
+
+        Console.WriteLine(result);
+    }
+}
